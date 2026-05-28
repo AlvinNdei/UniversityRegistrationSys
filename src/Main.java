1f928d51247c97ba8bc655c0a221d76ee03efccf
AlvinNdei <alvinmuriuki714@gmail.com>
2026-05-28 21:57:39 +0300

    Added a way to prompt the admin to add a student.

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,17 @@
 import storage.StudentsRepo;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String []args){
         StudentsRepo new_students = new StudentsRepo();
-        new_students.addStudents("Abigael","reg_no67994");
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Input your name: ");
+        String name=scanner.next();
+        System.out.println("Input your registration number: ");
+        String reg_no=scanner.next();
+        new_students.addStudents(name,reg_no);
         new_students.displayStudents();
     }
 }
