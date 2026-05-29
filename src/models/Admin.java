7822cba9e1db0732d6b22441728b8c6a36dd2e83
AlvinNdei <alvinmuriuki714@gmail.com>
2026-05-29 16:31:44 +0300

    Added validation

--- src/models/Admin.java
+++ src/models/Admin.java
@@ -11,8 +11,14 @@ public class Admin {
             Scanner fields=new Scanner(System.in);
             System.out.println("Input the name: ");
             String name=fields.next();
+            if (name==null||name.isBlank()){
+                System.out.println("Please input a value in the name field!!");
+            }
             System.out.println("input the registration number: ");
             String reg_no=fields.next();
+            if (reg_no==null||reg_no.isBlank()){
+                System.out.println("Please input a value in the Registration field!!");
+            }
             System.out.println("Student added Successfully!!");
         }else if (choice.equals("n")){
             System.out.println("Okay you may proceed");
