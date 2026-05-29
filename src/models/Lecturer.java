c9eec0d56d0d8d5afa766fef2a1b57c5e1beaaee
AlvinNdei <alvinmuriuki714@gmail.com>
2026-05-29 18:43:42 +0300

    Added functionality to add a lecturer

--- src/models/Lecturer.java
+++ src/models/Lecturer.java
@@ -5,19 +5,22 @@ import java.util.Map;
 import java.util.Scanner;
 
 public class Lecturer {
-    private final Map<String,String> staff=new HashMap<>();
-
+    private static final Map<String,String> staff=new HashMap<>();
+    static{
+        staff.put("Dr. Anton","001");
+        staff.put("Dr. Samuel","002");
+    }
     public void addLecturer(){
         System.out.println("Enter Lecturer's name:");
         Scanner fields=new Scanner(System.in);
         String name=fields.next();
         System.out.println("Enter Lecturer's ID:");
         String ID=fields.next();
+        staff.put(name,ID);
+        System.out.println(staff);
         System.out.println("Lecturer created successfully!!");
     }
     public void printStaff(){
-        this.staff.put("Dr. Anton","001");
-        this.staff.put("Dr. Samuel","002");
         System.out.println(staff);
     }
 }
